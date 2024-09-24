d4fe451357010200d17ec1632a2b0d4b252d7f7f
Theredhawksings <myeoseul@gmail.com>
2024-09-24 09:49:34 +0900

    전체적인 구조 완성
    으갸갸갸갹
    
    사각형 운동, 원 운동의 무한 루프 완성

---
+++ 2d.py
@@ -0,0 +1,31 @@
+from pico2d import *
+
+import os
+import math
+
+
+
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+
+
+def run_rectangle():
+    pass
+
+
+def run_circle():
+    pass
+
+    
+
+while(1):
+    clear_canvas_now()
+    run_rectangle()
+    run_circle()
+    
+
+close_canvas()
